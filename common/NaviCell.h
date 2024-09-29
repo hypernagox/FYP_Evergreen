c8c545f1082265df12703681293eca9cd9b94b0e
hypernagox <128601833+hypernagox@users.noreply.github.com>
2024-09-29 22:28:36 +0900

    pch수정

--- common/NaviCell.h
+++ common/NaviCell.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "pch.h"
+#include "DirectXCollision.h"
+#include "SimpleMath.h"
 
 class NaviCell
 {
--- common/pch.h
+++ common/pch.h
@@ -3,8 +3,6 @@
 #define WIN32_LEAN_AND_MEAN // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
 #define NOMINMAX
 
-#include "DirectXCollision.h"
-#include "SimpleMath.h"
 #include <execution>
 #include <string_view>
 #include <string>
