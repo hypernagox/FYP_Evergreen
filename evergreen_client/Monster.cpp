889a2a39759533fc37c24990dc83caad169217fd
Yup <wts1597@gmail.com>
2024-10-22 12:05:07 +0900

    Apply resource for Monster

--- evergreen_client/Monster.cpp
+++ evergreen_client/Monster.cpp
@@ -10,18 +10,17 @@ Monster::Monster(const std::shared_ptr<SceneObject>& object) : Component(object)
 
 	auto shader = INSTANCE(Resource)->Load<udsdx::Shader>(RESOURCE_PATH(L"color.hlsl"));
 	m_monsterMaterial = std::make_shared<udsdx::Material>();
-	m_monsterMaterial->SetMainTexture(INSTANCE(Resource)->Load<udsdx::Texture>(RESOURCE_PATH(L"Sprite-0001.png")));
+	m_monsterMaterial->SetMainTexture(INSTANCE(Resource)->Load<udsdx::Texture>(RESOURCE_PATH(L"fox\\fox_high_DefaultMaterial_BaseColor.png")));
 
 	m_transformBody = pBody->GetTransform();
 	m_rendererObj->AddChild(pBody);
 	m_rendererObj->GetTransform()->SetLocalPosition(Vector3::Up * -0.05f);
 
 	auto pBodyMesh = pBody->AddComponent<MeshRenderer>();
-	pBodyMesh->SetMesh(INSTANCE(Resource)->Load<udsdx::Mesh>(RESOURCE_PATH(L"fox_v1.fbx")));
+	pBodyMesh->SetMesh(INSTANCE(Resource)->Load<udsdx::Mesh>(RESOURCE_PATH(L"fox\\fox_low.fbx")));
 	pBodyMesh->SetShader(shader);
 	pBodyMesh->SetMaterial(m_monsterMaterial.get());
 
-	m_transformBody->SetLocalPosition(Vector3::Up * 0.8f);
 	m_transformBody->SetLocalScale(Vector3::One * 0.025f);
 
 	m_entityMovement = object->AddComponent<EntityMovement>();
