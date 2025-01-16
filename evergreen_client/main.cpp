2a6a0d8fc6c5aacc5b7c2db57473aed7bc37c848
hypernagox <hypernagox@tukorea.ac.kr>
2025-01-16 23:08:23 +0900

    using amazon aws server always on

--- evergreen_client/main.cpp
+++ evergreen_client/main.cpp
@@ -79,7 +79,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     {
         if constexpr (true == g_bUseDefaultIP)
         {
-            NET_NAGOX_ASSERT(NetMgr(NetworkMgr)->Connect<ServerSession>(L"127.0.0.1", 7777, s2c_PacketHandler::GetPacketHandlerList()));
+            NET_NAGOX_ASSERT(NetMgr(NetworkMgr)->Connect<ServerSession>(L"3.34.98.31", 7777, s2c_PacketHandler::GetPacketHandlerList()));
         }
         else
         {
