a2d22bd332e031fea90682d666055944ae5216cd
hypernagox <hypernagox@tukorea.ac.kr>
2025-07-01 07:24:22 +0900

    Change Quest index

--- evergreen_server/PartyQuestTable.cpp
+++ evergreen_server/PartyQuestTable.cpp
@@ -5,9 +5,6 @@
 void PartyQuestTable::InitPartyQuestTable() noexcept
 {
 	{
-		g_party_quest_table.emplace_back(NagiocpX::MakeShared<InvadeQuest_1>);
-	}
-	{
 		g_party_quest_table.emplace_back(NagiocpX::MakeShared<TutorialGuardQuest>);
 	}
 	{
